c91135e06292cef57e058951975f7ed30dd9bbb7
nguyenmya <80749352+nguyenmya@users.noreply.github.com>
2021-10-07 10:44:18 +0700

    delete

--- src/components/Cau1/Cau1.jsx
+++ src/components/Cau1/Cau1.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 export default function List() {
     return (
         <div className="container">
-            Khi truy cập trang web, trình duyệt sẽ gọi tới máy chủ DNS để biên dịch URL trang web thành một địa chỉ IP, mỗi trang web có địa chỉ IP riêng biệt. Khi tìm thấy địa chỉ IP của trang web chúng ta đang vào, địa chỉ IP đó sẽ được trả về cho trình duyệt.
         </div>
     )
 }
--- src/components/Cau3/Cau3.jsx
+++ src/components/Cau3/Cau3.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 export default function Cau3() {
     return (
         <div>
-            thứ tự ưu tiên inline --- id---  class-- các thẻ
         
         </div>
     )
